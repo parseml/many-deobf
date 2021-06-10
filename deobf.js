a124ef8e196eff008e34c3fc34e0ee02e7174703
Parse <41178513+parseml@users.noreply.github.com>
2021-06-10 15:29:00 -0400

    Update deobf.js

--- deobf.js
+++ deobf.js
@@ -1,11 +1,18 @@
-// Variable Finder (Deobfuscator)
-
 consoleref.log( // Please do not remove!
 	'%cDeobfuscator created by Parse\nCheck it out -> github.com/parseml/many-deobf',
-	'background: green; color: white; display: block; padding-left: 0px;',
+	'background: purple; color: white; display: block; padding: 10px;',
 );
 
-const Deobfuscator = {
+consoleref.log(
+	'%cHave Discord? Join the server! -> https://discord.gg/aDdRVMzqfK',
+	'background: purple; color: white; display: block; padding: 10px;',
+)
+
+/*
+ * I will be updating the Deobfuscator object soon to make it more efficient.. 
+ * If you get any issues feel free to open an issue with GitHub or in our Discord server.
+*/
+var Deobfuscator = {
 	function: (object, string, returnKey) => {
 		let keyFound = null;
 
@@ -25,6 +32,7 @@ const Deobfuscator = {
 
 		return keyFound;
 	},
+
 	object: (object, string, returnKey) => {
 		let keyFound = null;
 
@@ -44,6 +52,7 @@ const Deobfuscator = {
 
 		return keyFound;
 	},
+
 	variableByLength: (object, length, returnKey) => {
 		let keyFound = null;
 
@@ -63,6 +72,7 @@ const Deobfuscator = {
 
 		return keyFound;
 	},
+
 	keyBetween: (func, start, end) => {
 		let keyFound = null;
 
@@ -75,6 +85,7 @@ const Deobfuscator = {
 
 		return keyFound;
 	},
+
 	findByType: (object, type, returnKey) => {
 		let keyFound = null;
 
@@ -90,63 +101,36 @@ const Deobfuscator = {
 	}
 };
 
-// Obfuscation List
-
+/*
+ * Predefined objects, functions & variables for Manyland
+ * Feel free to use this as a reference for finding others.
+*/
 ig.game.player = Deobfuscator.object(ig.game, "screenName", false);
 ig.game.player.changeName = Deobfuscator.function(ig.game.player, "this.screenName", false);
 ig.game.players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", false).player;
 ig.game.equip = Deobfuscator.object(ig.game, "removeItemFromCollection", false);
-ig.game.equip.item = Deobfuscator.function(ig.game.equip, "AnimationSheet(null,d", false);
+ig.game.equip.item = Deobfuscator.function(ig.game.equip, "getItem_P", false);
+ig.game.player.allowEquip = Deobfuscator.keyBetween(ig.game.init, "t=!0,ig.game.", "=!0);");
+ig.game[ig.game.player.allowEquip] = true;
 ig.game.blocks = Deobfuscator.object(ig.game, "lastRequestTimestamps", false);
 ig.game.websocket = Deobfuscator.object(ig.game, "binary", false);
 ig.game.player.id = Deobfuscator.variableByLength(ig.game.player, 24, false);
 id = Deobfuscator.variableByLength(ig.game.player, 24, true);
 
-// Functions you can use to speed up programming
-
-const idFromScreenName = screenName => {
+/*
+ * Function to find someones RID from their screenName in game.
+*/
+function idFromScreenName(screenName) {
 	updatePlayers();
-	const key = Deobfuscator.variableByLength(ig.game.player, 24, true);
-	return new Promise((res, rej) => {
-		ig.game.players.forEach(player => {
-			if(player.screenName === screenName) {
-				res(player[key]);
-			}
-		});
 
-		rej('Player not found!');
-	});
+	return ig.game.players.filter(i =>
+		i.screenName == screenName
+	)[0]['id']
 }
 
-const updatePlayers = () => {
+/*
+ * Function to update the ig.game.player array
+*/
+function updatePlayers() {
 	ig.game.players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", false).player;
-}
-
-const getPlayerChat = target => {
-	updatePlayers();
-	let chat = "";
-
-	ig.game.players.forEach(player => {
-		if(player[id] === target) {
-			let playerChat = Deobfuscator.object(player, 'player', false);
-			playerChat.object = Deobfuscator.findByType(playerChat, Array, false);
-
-			if(playerChat.object.length != 0) {
-				let index = playerChat.object.length - 1;
-				chat = Deobfuscator.findByType(playerChat.object[index], String, false);
-			}
-		}
-	});
-
-	return chat;
-}
-
-const itemInformation = async id => {
-	const api = 'http://idf.manyland.netdna-cdn.com/';
-	return new Promise((res, rej) => {
-		$.get(api + id, (data, code) => {
-			if(code === 'success') res(data);
-			else rej('Something went wrong!');
-		});
-	});
 }
