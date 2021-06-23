1e56b78ba1db467dcdfa97b99f3f231a282812a5
Parse <41178513+parseml@users.noreply.github.com>
2021-06-23 19:09:51 -0400

    Updated for faster "deobfuscation".
    Faster deobfuscation
    Removed Deobfuscator.findByType
    Removed advertisement console.log

--- deobf.js
+++ deobf.js
@@ -1,136 +1,100 @@
-consoleref.log( // Please do not remove!
+/**
+ * Written by Parse
+ * A tool for finding minified manyland variables.
+ */
+consoleref.log(
 	'%cDeobfuscator created by Parse\nCheck it out -> github.com/parseml/many-deobf',
 	'background: purple; color: white; display: block; padding: 10px;',
 );
 
-consoleref.log(
-	'%cHave Discord? Join the server! -> https://discord.gg/aDdRVMzqfK',
-	'background: purple; color: white; display: block; padding: 10px;',
-)
-
-/*
- * I will be updating the Deobfuscator object soon to make it more efficient.. 
- * If you get any issues feel free to open an issue with GitHub or in our Discord server.
-*/
+/**
+ * I'm too lazy to document this, but gladly someone else did it for me.
+ * You can check it out here for more information.
+ * https://manylandmods.github.io/manyland-documentation/#/obfuscation?id=parses-deobfuscator
+ */
 var Deobfuscator = {
-	function: (object, string, returnKey) => {
-		let keyFound = null;
+	object: function(object, string, returnKey) {
+		let keys = Object.keys(object)
+		let proto = Object.keys(Object.getPrototypeOf(object))
 
-		Object.keys(object).forEach((i) => {
-			if(!object[i]) return;
+		test = [ ...keys, ...proto ]
 
-			if(object[i].toString().includes(string))
-				keyFound = returnKey ? i : object[i];
-		});
+		for(let key of [ ...keys, ...proto ]) {
+			if(!object[key])
+				continue
 
-		Object.keys(Object.getPrototypeOf(object)).forEach((i) => {
-			if(!object[i]) return;
+			let _keys = Object.keys(object[key])
+			let _proto = Object.keys(Object.getPrototypeOf(object[key]))
 
-			if(object[i].toString().includes(string))
-				keyFound = returnKey ? i : object[i];
-		});
-
-		return keyFound;
+			if([ ..._keys, ..._proto ].includes(string))
+				return returnKey ? key : object[key]
+		}
 	},
 
-	object: (object, string, returnKey) => {
-		let keyFound = null;
+	function: function(object, string, returnKey) {
+		let keys = Object.keys(object)
+		let proto = Object.keys(Object.getPrototypeOf(object))
 
-		Object.keys(object).forEach((i) => {
-			if(!object[i]) return;
+		for(let key of [ ...keys, ...proto ]) {
+			if(!object[key])
+				continue
 
-			if(Object.keys(object[i]).includes(string))
-				keyFound = returnKey ? i : object[i];
-		});
-
-		Object.keys(Object.getPrototypeOf(object)).forEach((i) => {
-			if(!object[i]) return;
-
-			if(Object.keys(Object.getPrototypeOf(object[i])).includes(string))
-				keyFound = returnKey ? i : object[i];
-		});
-
-		return keyFound;
+			if(object[key].toString().includes(string))
+				return returnKey ? key : object[key]
+		}
 	},
 
-	variableByLength: (object, length, returnKey) => {
-		let keyFound = null;
+	variableByLength: function(object, length, returnKey) {
+		let keys = Object.keys(object)
+		let proto = Object.keys(Object.getPrototypeOf(object))
 
-		Object.keys(object).forEach((i) => {
-			if(!object[i]) return;
+		for(let key of [ ...keys, ...proto ]) {
+			if(!object[key])
+				continue
 
-			if(object[i].length === length)
-				keyFound = returnKey ? i : object[i];
-		});
-
-		Object.keys(Object.getPrototypeOf(object)).forEach((i) => {
-			if(!object[i]) return;
-
-			if(object[i].length === length)
-				keyFound = returnKey ? i : object[i];
-		});
-
-		return keyFound;
+			if(object[key].length === length)
+				return returnKey ? key : object[key]
+		}
 	},
 
-	keyBetween: (func, start, end) => {
-		let keyFound = null;
-
-		const str = func.toString();
-
-		keyFound = str.substring(
-    		str.lastIndexOf(start) + start.length, 
-    		str.lastIndexOf(end)
-		);
-
-		return keyFound;
-	},
-
-	findByType: (object, type, returnKey) => {
-		let keyFound = null;
-
-		Object.keys(object).forEach((i) => {
-			if(object[i] === null)
-				return;
-
-			if(object[i].constructor === type)
-				keyFound = returnKey ? i : object[i];
-		});
-
-		return keyFound;
+	keyBetween: function(func, start, stop) {
+		return func.toString().substring(
+			func.toString().lastIndexOf(start) + start.length,
+			func.toString().lastIndexOf(stop)
+		)
 	}
 };
 
-/*
- * Predefined objects, functions & variables for Manyland
- * Feel free to use this as a reference for finding others.
-*/
-ig.game.player = Deobfuscator.object(ig.game, "screenName", false);
-ig.game.player.changeName = Deobfuscator.function(ig.game.player, "this.screenName", false);
-ig.game.players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", false).player;
-ig.game.equip = Deobfuscator.object(ig.game, "removeItemFromCollection", false);
-ig.game.equip.item = Deobfuscator.function(ig.game.equip, "getItem_P", false);
-ig.game.player.allowEquip = Deobfuscator.keyBetween(ig.game.init, "t=!0,ig.game.", "=!0);");
-ig.game[ig.game.player.allowEquip] = true;
-ig.game.blocks = Deobfuscator.object(ig.game, "lastRequestTimestamps", false);
-ig.game.websocket = Deobfuscator.object(ig.game, "binary", false);
-ig.game.player.id = Deobfuscator.variableByLength(ig.game.player, 24, false);
-id = Deobfuscator.variableByLength(ig.game.player, 24, true);
+/**
+ * Here is an example of getting minified variables.
+ * These are also pre-defined variables that you can use in your mods / scripts!
+ */
+ig.game.player = Deobfuscator.object(ig.game, 'screenName', false)
+id = Deobfuscator.variableByLength(ig.game.player, 24, true)
+ig.game.player.id = ig.game.player[id]
+ig.game.player.changeName = Deobfuscator.function(ig.game.player, 'this.screenName=', false)
+ig.game.players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", false).player
+ig.game.equip = Deobfuscator.object(ig.game, "getCollectedItemsForPlayer", false)
+ig.game.equip.item = Deobfuscator.function(ig.game.equip, "getItem_P", false)
+ig.game.blocks = Deobfuscator.object(ig.game, "lastRequestTimestamps", false)
+ig.game.websocket = Deobfuscator.object(ig.game, "binary", false)
+ig.game.player.id = Deobfuscator.variableByLength(ig.game.player, 24, false)
+players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", true)
+allowEquiping = Deobfuscator.keyBetween(ig.game.init, "t=!0,ig.game.", "=!0);")
+ig.game[allowEquiping] = true
 
 /*
  * Function to find someones RID from their screenName in game.
-*/
+ */
 function idFromScreenName(screenName) {
-	updatePlayers();
+	updatePlayers()
 
-	return ig.game.players.filter(i =>
-		i.screenName == screenName
-	)[0]['id']
+	return ig.game.players.filter(i => i.screenName == screenName)[0]
 }
 
 /*
- * Function to update the ig.game.player array
-*/
+ * Function to update the ig.game.player array for new players
+ */
 function updatePlayers() {
-	ig.game.players = Deobfuscator.object(ig.game, "betweenDefaultAndPlayer", false).player;
+	ig.game.players = ig.game[players].player
 }
